8c01bcf1331294d543d3699dfaefbc95f6b67cba
mashuai <mashuai@yidian-inc.com>
2024-03-08 11:44:38 +0800

    fix: 加解密注意事项!!! 特别是字符串(以此commit为准)

--- demo.cc
+++ demo.cc
@@ -22,7 +22,8 @@ static void read_hex(const char *in, uint8_t* v, size_t size, const char* param_
 */ 
 int main(int argc, char const *argv[]) {
     /* code */
-    //sizeof(TEST_PLAIN) 有 \0, 多一个字节，所以加密是传 strlen(TEST_PLAIN)=13
+    //sizeof(TEST_PLAIN) 有 \0, 多一个字节，strlen(TEST_PLAIN)=13，所以要加密sizeof(TEST_PLAIN)，把\0也加密,
+    //最好的方式是加密原数据也变成16的倍数，不够的补\0 !!!
     char TEST_PLAIN[] = "hello world !";
     // char TEST_PLAIN[13] = {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', ' ', '!'};
     // char TEST_PLAIN[] = "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710";
@@ -52,28 +53,31 @@ int main(int argc, char const *argv[]) {
     // printf("Decrypt, vector #1 ret:%d\n", memcmp(output, plain, sizeof(plain)));
 
     // uint8_t cipher1[16] = {0};
-    uint32_t left = strlen(TEST_PLAIN) % 16;
+    uint32_t left = sizeof(TEST_PLAIN) % 16;
     uint32_t padding = (16 - left);
-    size_t block = strlen(TEST_PLAIN) + padding;
+    size_t block = sizeof(TEST_PLAIN) + padding;
     uint8_t *cipher1 = (uint8_t *) malloc(block);
     memset(cipher1, 0, block);
-
-    // (*encrypt)(iv_or_nonce, (uint8_t *)TEST_PLAIN, sizeof(TEST_PLAIN), cipher1);
-    (*encrypt)(iv_or_nonce, (uint8_t *)TEST_PLAIN, strlen(TEST_PLAIN), cipher1);
+    (*encrypt)(iv_or_nonce, (uint8_t *)TEST_PLAIN, sizeof(TEST_PLAIN), cipher1);
 
     // uint8_t origin[16] = {0};
     uint8_t *origin = (uint8_t *) malloc(block);
     memset(origin, 0, block);
-    // (*decrypt)(iv_or_nonce, cipher1, sizeof(cipher1), origin);
     (*decrypt)(iv_or_nonce, cipher1, block, origin);
 
-    //非常重要!!! whitebox encrypt/decrypt 接口没有返回字节数，按 cipher's block，结尾会有乱码 origin="hello world !t";
+    //非常重要!!! whitebox encrypt/decrypt 接口没有返回字节数，按 cipher's block，结尾会有乱码 ext:origin="hello world !t";
     std::string ret((char *)origin, strlen(TEST_PLAIN));
     std::cout << "- ret:" << ret << std::endl;
+    //由于加密的是sizeof(TEST_PLAIN)，包含\0，所以下面的转字符串没有问题
+    std::string ret1((const char *)origin);
 
     // for (int i = 0; i < sizeof(TEST_PLAIN); i++) {
-    for (int i = 0; i < strlen(TEST_PLAIN); i++) {
-        printf("%c", origin[i]);
+    for (int i = 0; i < block; i++) {
+        if (origin[i] == '\0') {
+            printf("++++++++++++++++++\n");
+        }
+        
+        printf("%c\n", origin[i]);
     }
 
     printf("\n");
